37bb11e47a4cda74c604cca9017a03bd30160d0b
Mike Zamansky <zamansky@gmail.com>
2013-10-07 09:58:54 -0400

    Z

--- coin/Coin.java
+++ coin/Coin.java
@@ -4,6 +4,7 @@ public class Coin {
     private int tosses;
     private int heads;
     private double prob = 0.5; // another way to initialize
+    private int value;
 
     public void initCoin(String f, Double p) {
 	face = f;
@@ -12,6 +13,10 @@ public class Coin {
 	prob=p;
     }
 
+    public Coin(int v) {
+	value = v;
+	initCoin("Heads",0.5);
+    }
 
     public Coin() {
 	initCoin("Heads",0.5);
@@ -29,5 +34,13 @@ public class Coin {
     public boolean equals(Coin other) {
 	return face.equals(other.getFace());
     }
-    
+
+    public int getValue() {
+	return value;
+    }
+
+    public String toString() {
+	return ""+value+" cents";
+    }
+
 }
--- coin/Driver.java
+++ coin/Driver.java
@@ -1,32 +1,17 @@
 
 public class Driver {
     public static void main(String[] args) {
-	Coin head1 = new Coin();
-	String s = new String("Tails");
-	Coin tail = new Coin(s);
-	s = new String("Heads");
-	Coin head2 = new Coin(s);
-	
-	System.out.println(head1.getFace()+" "+tail.getFace()+" "+head2.getFace());
+	Coin q = new Coin(25);
+	Coin d = new Coin(10);
+	Coin n = new Coin(5);
 
-	System.out.println(head1.equals(tail));
-	System.out.println(head1.equals(head2));
-	System.out.println(tail.equals(head2));
-	System.out.println(head2.equals(head1));
-	
-	System.out.println();
-	
-	String s1 = "abcde";
-	String s2 = "defghi";
-	
-	System.out.println(s1.compareTo(s2));
-	System.out.println(s2.compareTo(s1));
-	System.out.println(s1.compareTo(s1));
-	
-	System.out.println("a".compareTo("b"));
-	System.out.println("b".compareTo("a"));
-	System.out.println("A".compareTo("a"));
+	System.out.println(q.getValue());
+	System.out.println(q);
+	System.out.println(d);
+	System.out.println(n);
 
+	String s1 = "The Coin q is "+q;
+	System.out.println(s1);
 
 
     }
