6295e0b84ebc659bcd1987ef0d724c9c512ee87a
Mike Zamansky <zamansky@gmail.com>
2013-10-24 09:20:45 -0400

    Z

--- pd2/stuyabloII/Wizard.java
+++ pd2/stuyabloII/Wizard.java
@@ -1,4 +1,12 @@
 public class Wizard  extends Character{
+    protected int mana;
+
+    
+    public Wizard(String name) {
+	super(name);
+	mana = 20;
+    }
+    
     public String toString() {
 	return super.toString()+" the frog";
     }
