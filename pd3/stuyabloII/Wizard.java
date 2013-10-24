e7d31dbe616147544220ab7190c4456c3540bace
Mike Zamansky <zamansky@gmail.com>
2013-10-24 10:05:25 -0400

    Super

--- pd3/stuyabloII/Wizard.java
+++ pd3/stuyabloII/Wizard.java
@@ -1,6 +1,13 @@
 
 public class Wizard extends Character{
+    private int mana;
+
+    public Wizard(String name) {
+	super(name);
+	mana = 20;
+    }
+
     public String toString() {
-	return super.toString()+" the wise";
+	return super.toString()+" the wise who has "+mana+" mana";
     }
 }
